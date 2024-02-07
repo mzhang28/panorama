6db9469d8e2745d155884308057d41dfca7a627e
Michael Zhang <mail@mzhang.io>
2024-02-07 11:48:25 -0600

    update

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use cozo::{DbInstance, ScriptMutability};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -6,6 +8,13 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let db = DbInstance::new("mem", "", Default::default()).unwrap();
+    let script = "?[a] := a in [1, 2, 3]";
+    let result = db
+        .run_script(script, Default::default(), ScriptMutability::Immutable)
+        .unwrap();
+    println!("{:?}", result);
+
     tauri::Builder::default()
         .plugin(tauri_plugin_window::init())
         .plugin(tauri_plugin_shell::init())
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,9 @@
+import styles from "./Sidebar.module.scss";
+
+export default function Sidebar() {
+  return (
+    <div class={styles.sidebar}>
+      <h1>Panorama</h1>
+    </div>
+  );
+}
