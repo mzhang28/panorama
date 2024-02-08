1b4e74485b2a363ee5b76102e855c1587105e92f
Michael Zhang <mail@mzhang.io>
2024-02-07 23:09:11 -0600

    akj

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,6 +4,10 @@ export default function Sidebar() {
   return (
     <div class={styles.sidebar}>
       <h1>Panorama</h1>
+
+      <h3>Bookmarked Nodes</h3>
+
+      <h3>Settings</h3>
     </div>
   );
 }
