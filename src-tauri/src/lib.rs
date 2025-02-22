ede20e185817677f821aa562e7fa002d051f3db6
Michael Zhang <mail@mzhang.io>
2025-02-22 01:14:21 -0600

    some tauri stuff

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,16 +1,22 @@
 use panorama_backend::{create_context, create_web_server};
-use tokio::net::TcpListener;
+use tauri::tray::TrayIconBuilder;
+
+#[tauri::command]
+fn api_fetch() {}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
+    .plugin(tauri_plugin_http::init())
     .setup(|app| {
-      tauri::async_runtime::spawn(async {
+      async {
         let context = create_context().await.unwrap();
         let app = create_web_server(context).await.unwrap();
-        let listener = TcpListener::bind("0.0.0.0:10203").await.unwrap();
-        axum::serve(listener, app).await
-      });
+        // let listener = TcpListener::bind("0.0.0.0:10203").await.unwrap();
+        // axum::serve(listener, app).await
+      };
+
+      let tray = TrayIconBuilder::new().build(app)?;
 
       if cfg!(debug_assertions) {
         app.handle().plugin(
@@ -21,6 +27,7 @@ pub fn run() {
       }
       Ok(())
     })
+    .invoke_handler(tauri::generate_handler![api_fetch])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
