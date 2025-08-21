205cefadbf2b3f6b9753e934f62f05123b951332
Michael Zhang <mail@mzhang.io>
2025-08-21 00:59:37 -0400

    daemon

--- ui/main.cpp
+++ ui/main.cpp
@@ -1,12 +1,28 @@
 #include "MainWindow.h"
 #include <QApplication>
+#include <QCommandLineParser>
 
 int main(int argc, char *argv[]) {
   QApplication app(argc, argv);
 
-  MainWindow window;
-  window.setWindowTitle("Panorama");
-  window.show();
+  QCommandLineParser parser;
+  parser.setApplicationDescription("panorama");
+  parser.addHelpOption();
 
-  return app.exec();
+  QCommandLineOption daemonOption("daemon", "Run as background daemon");
+  parser.addOption(daemonOption);
+  parser.process(app);
+
+  if (parser.isSet(daemonOption)) {
+    qDebug() << "Running as daemon...";
+    // start your daemon loop here
+    return app.exec();
+  } else {
+    qDebug() << "Running GUI...";
+    MainWindow window;
+    window.setWindowTitle("Panorama");
+    window.show();
+
+    return app.exec();
+  }
 }
