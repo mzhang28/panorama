56922c3c2b70d14b873af7775753c6686d6578f8
Michael Zhang <mail@mzhang.io>
2025-12-22 15:32:40 -0500

    some scaffolding

---
+++ src/routes/__root.tsx
@@ -0,0 +1,21 @@
+import Layout from "@/layout";
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+
+const RootLayout = () => (
+  <Layout>
+    {/*<div className="p-2 flex gap-2">
+      <Link to="/" className="[&.active]:font-bold">
+        Home
+      </Link>{" "}
+      <Link to="/about" className="[&.active]:font-bold">
+        About
+      </Link>
+    </div>
+    <hr />*/}
+    <Outlet />
+    <TanStackRouterDevtools />
+  </Layout>
+);
+
+export const Route = createRootRoute({ component: RootLayout });
